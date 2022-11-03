9d3fdc9fc6fa6ef6621005a3b04dac6a9b64a7a8
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-02 18:20:08 -0700

    starter code

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (name)
+(`sales`, ),
+(`hr`, ),
+(`management`, ),
+(`security`, ),
+(`marketing`, ),
+(`customer service`, );
+
+INSERT INTO  role (title, salary, department_id)
+(`salesman`, ``, ``),
+(`recruiter`, ``, ``),
+(`manager`, ``, ``),
+(`officer`, ``, ``),
+(`assisant`, ``, ``),
+(`associate`, ``, ``);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+(`Alexis`, `Smith`, `salesman`, ``),
+(`Kayla`, `Jones`, `recruiter`, ``),
+(`Jesse`, `Martinez`, `manager`, ``),
+(`Mark`, `Johnson`, `officer`, ``),
+(`David`, `Garcia`, `assistant`, ``),
+(`Sarah`, `Sanchez`, `associate`, ``);
